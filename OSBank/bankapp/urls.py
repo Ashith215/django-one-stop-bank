77b9fdeb63ba3cbef8d24d4a7a76c3b6bad76a83
Ashith215 <me@gmail.com>
2025-01-07 22:10:04 +0530

    url changed

--- OSBank/bankapp/urls.py
+++ OSBank/bankapp/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
 ]
 
 
-# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 
